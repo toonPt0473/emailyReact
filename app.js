211da71f541b512576938df357f0ff862f2a03cf
Paitoon Arayasatjapong <rimb9pkmink@gmail.com>
2017-08-23 12:09:16 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/' , (req , res) => {
+    res.send({hi : 'there'});
+});
+
+app.listen(process.env.PORT || 8080 , () => {
+    console.log('server started')
+})
