ca93d1ab41f715c28c3a772b0915207e34a17015
Paitoon Arayasatjapong <rimb9pkmink@gmail.com>
2017-08-25 22:16:32 +0700

    change callback url

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,7 @@ passport.use(
     new googleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret ,
-        callbackURL: (NODE_ENV === 'production') ? 'https://toonpt.herokuapp.com/auth/google' : '/auth/google'
+        callbackURL: (process.env.NODE_ENV === 'production') ? 'https://toonpt.herokuapp.com/auth/google' : '/auth/google'
     }, (accessToken , refreshToken , profile , done) => {
         User.findOne({googleID : profile.id})
             .then(existUser => {
