edf0969eaafd95e05b1a7704d3280b11ffc90d40
Paitoon Arayasatjapong <rimb9pkmink@gmail.com>
2017-09-01 13:47:45 +0700

    change path of directory

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoute')(app);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+    app.use(express.static('/client/build'));
     const path = require('path');
     app.get('*' , (req , res)  => {
         res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'))
